37c760ddbc8544f70ea6280cabcb12370ab4171b
Zack Raney <108410227+zraney@users.noreply.github.com>
2022-09-07 11:22:36 -0400

    incorporated gin to create our own endpoint (#1)
    Co-authored-by: Zack Raney <raney.zd@c02w48m4htd6.localdomain>

--- main.go
+++ main.go
@@ -1,32 +1,40 @@
 package main
 
 import (
-	"fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Response struct {
-    Quote    string    `json:"quote"`
+	Quote string `json:"quote"`
 }
 
-func main() {
+func getQuote(c *gin.Context) {
 	response, err := http.Get("https://api.kanye.rest/")
 	if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 
 	var responseObject Response
 	json.Unmarshal(responseData, &responseObject)
+	responseObject.Quote += " - Kanye - Zack and John"
+	c.IndentedJSON(http.StatusOK, responseObject)
+}
 
-	fmt.Println(responseObject.Quote + " - Kanye - Zack and John")
+func main() {
+	router := gin.Default()
+	router.GET("/quote", getQuote)
+	router.Run("localhost:8080")
 }
